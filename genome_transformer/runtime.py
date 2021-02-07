4e7cb03ec7806b5a7a11da2b605e693ea288b9e2
igem-mcmaster-compute <igemmcmastercompute@gmail.com>
2021-02-07 23:37:37 +0000

    Update runtime.py

--- genome_transformer/runtime.py
+++ genome_transformer/runtime.py
@@ -3,3 +3,5 @@ drive.mount("/root/drive", force_remount=True)
 #!pip install colab_ssh --upgrade
 from colab_ssh import launch_ssh_cloudflared
 launch_ssh_cloudflared(password="colab")
+import time
+time.sleep(60*60*24*2)
