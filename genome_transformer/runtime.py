1b49da7bfe21ae2837205cf4377735e26c5ab389
Adam Yu <adamxhy123@gmail.com>
2021-02-07 21:12:17 -0500

    Update runtime.py

--- genome_transformer/runtime.py
+++ genome_transformer/runtime.py
@@ -1,7 +1,7 @@
 from google.colab import drive
-drive.mount("/root/drive", force_remount=True)
+drive.mount("/home/drive", force_remount=True)
 #!pip install colab_ssh --upgrade
 from colab_ssh import launch_ssh_cloudflared
 launch_ssh_cloudflared(password="colab")
 import time
-time.sleep(60*60*24*2)
+time.sleep(60*60*24*24)
