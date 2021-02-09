a2bba941dacd6f8b46621eb7b2e80628c0e7610f
igem-mcmaster-compute <igemmcmastercompute@gmail.com>
2021-02-09 01:56:13 +0000

    Update runtime.py

--- genome_transformer/runtime.py
+++ genome_transformer/runtime.py
@@ -1,3 +1,11 @@
+"""
+SSH into Colab instance.
+
+Launch and sustain idle a Colab notebook for 24 days.
+Uncomment the `pip install` statement.
+Execute the code in a Colab notebook.
+Follow instructions from cell output.
+"""
 from google.colab import drive
 drive.mount("/home/drive", force_remount=True)
 #!pip install colab_ssh --upgrade
