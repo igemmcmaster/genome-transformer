aabeb19e3021d30e73f04f97bdaf6c65e011117d
Lev1ty <adamxhy123@outlook.com>
2021-02-14 00:11:33 +0000

    Update make_tokenizer.py

--- genome_transformer/make_tokenizer.py
+++ genome_transformer/make_tokenizer.py
@@ -1,4 +1,38 @@
-import transformers
+import itertools
 
-tokenizer = transformers.PreTrainedTokenizerFast()
-import transf
+import tokenizers
+
+from absl import app, flags
+
+FLAGS = flags.FLAGS
+flags.DEFINE_integer("k", None, "Number of nucleotides for k-mer sequence generation", lower_bound=1)
+flags.DEFINE_string("savepath", None, "Path to save tokenizer parameters")
+
+nucleotides = ["a", "c", "g", "t"]
+unknown = "x"
+padding = "p"
+extras = [padding, unknown]
+
+
+def make_tokenizer(k: int):
+    """
+    Make tokenizer for k-mer gene sequences.
+    """
+    keys = extras + list("".join(token) for token in itertools.product(*(nucleotides for _ in range(k))))
+    values = range(len(keys))
+    vocab = dict(zip(keys, values))
+    tokenizer = tokenizers.Tokenizer(tokenizers.models.WordLevel(vocab=vocab, unk_token=unknown))
+    tokenizer.enable_padding(pad_token="p")
+    tokenizer.pre_tokenizer = tokenizers.pre_tokenizers.WhitespaceSplit()
+    return tokenizer
+
+
+def main(argv):
+    del argv
+    tokenizer = make_tokenizer(FLAGS.k)
+    if FLAGS.savepath is not None:
+        tokenizer.save(FLAGS.savepath, pretty=True)
+
+
+if __name__ == "__main__":
+    app.run(main)
