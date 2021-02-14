cd40c0a91311e7f748c76f2f1e45e4bc3a0ced63
Lev1ty <adamxhy123@outlook.com>
2021-02-14 01:53:33 +0000

    Update extract_seq.py

--- genome_transformer/extract_seq.py
+++ genome_transformer/extract_seq.py
@@ -6,8 +6,10 @@ from pathlib import Path
 
 import dask.bag as db
 import dask.dataframe as dd
+import pandas as pd
 from absl import app, flags
 from Bio import SeqIO
+from dask.diagnostics import ProgressBar
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("datadir", None, "Path to data directory containing *.gbff.gz files")
@@ -15,15 +17,15 @@ flags.DEFINE_string("savepath", None, "Path to save DataFrame")
 
 
 def extract_seq(path: str):
-    return Path(path).exists()
     with gzip.open(path, "rt") as f:
-        return SeqIO(f, "genbank").seq
+        return next(SeqIO.parse(f, "genbank")).seq
 
 
 def main(argv):
     del argv
+    ProgressBar().register()
     df = db.from_sequence(str(datapath) for datapath in Path(FLAGS.datadir).iterdir() if ".gbff" in datapath.suffixes).to_dataframe({"datapath": str})
-    df.sequence = df.datapath.map(extract_seq)
+    df["sequence"] = df.datapath.map(extract_seq, meta=pd.Series(dtype=str))
     df.to_csv(FLAGS.savepath)
 
 
