2ee273db4d0110426696f89c95e1a8f70ef93896
Lev1ty <adamxhy123@outlook.com>
2021-02-14 01:44:06 +0000

    Update extract_seq.csv

--- genome_transformer/extract_seq.py
+++ genome_transformer/extract_seq.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
-# Example usage: ./extract_seq.py -datadir /content/drive/Shareddrives/mGEM\ R\&D/viral/2021-02-10_02-37-27/files
+# Example usage: ./extract_seq.py -datadir /content/drive/Shareddrives/mGEM\ R\&D/viral/2021-02-10_02-37-27/files -savepath seq.csv
 
 import gzip
 from pathlib import Path
 
+import dask.bag as db
+import dask.dataframe as dd
 from absl import app, flags
 from Bio import SeqIO
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("datadir", None, "Path to data directory containing *.gbff.gz files")
+flags.DEFINE_string("savepath", None, "Path to save DataFrame")
 
 
-def extract_seq(path: Path):
+def extract_seq(path: str):
+    return Path(path).exists()
     with gzip.open(path, "rt") as f:
         return SeqIO(f, "genbank").seq
 
 
 def main(argv):
     del argv
-    datapaths = (datapath for datapath in Path(FLAGS.datadir).iterdir() if ".gbff" in datapath.suffixes)
-    for datapath in datapaths:
-        print(datapath)
+    df = db.from_sequence(str(datapath) for datapath in Path(FLAGS.datadir).iterdir() if ".gbff" in datapath.suffixes).to_dataframe({"datapath": str})
+    df.sequence = df.datapath.map(extract_seq)
+    df.to_csv(FLAGS.savepath)
 
 
 if __name__ == "__main__":
